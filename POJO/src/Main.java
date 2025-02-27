a23e40cec61ab9f3898270102c2e90bcc8651749
Ioan Ilie <ionutilie2002@gmail.com>
2025-02-27 13:32:14 +0200

    THE POJO CLASS

---
+++ POJO/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            Student s = new Student("S92300" + i,
+                    switch (i)
+                    {
+                        case 1 -> "Mary";
+                        case 2 -> "Teo";
+                        case 3 -> "Alin";
+                        case 4-> "Ioan";
+                        case 5 -> "Lisa";
+                        default -> "Anonymus";
+                    },
+                    "01.03.2002",
+                    "Java Masterclass"
+                    );
+            System.out.println(s);
+
+        }
+
+    }
+}
