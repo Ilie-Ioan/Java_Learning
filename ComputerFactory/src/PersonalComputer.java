648546f6dcf3ed161c180705d2ab0da90e40452f
Ioan Ilie <ionutilie2002@gmail.com>
2025-03-08 11:15:47 +0200

    Composition learning and practicing

---
+++ ComputerFactory/src/PersonalComputer.java
@@ -0,0 +1,40 @@
+public class PersonalComputer extends Product{
+
+    private Monitor monitor;
+    private Motherboard motherboard;
+    private ComputerCase computerCase;
+
+    public PersonalComputer(String model, String manufacturer, Monitor monitor, Motherboard motherboard, ComputerCase computerCase) {
+        super(model, manufacturer);
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+        this.computerCase = computerCase;
+    }
+
+    public void drawLogo() {
+        monitor.drawPixel(1200,50,"yellow");
+
+    }
+
+    public void powerButton()
+    {
+        computerCase.pressPowerButton();
+        drawLogo();
+    }
+
+
+
+//    public Monitor getMonitor() {
+//        return monitor;
+//    }
+//
+//    public Motherboard getMotherboard() {
+//        return motherboard;
+//    }
+//
+//    public ComputerCase getComputerCase() {
+//        return computerCase;
+//    }
+
+
+}
