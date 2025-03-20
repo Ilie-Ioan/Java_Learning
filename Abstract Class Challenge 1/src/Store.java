6063b923764d8702f887a7adc9a51eca896683a5
Ioan Ilie <ionutilie2002@gmail.com>
2025-03-20 11:31:44 +0200

    Abstact class practice + abstract class challenge

---
+++ Abstract Class Challenge 1/src/Store.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+record OrderItem(int qty, ProductForSale product) {}
+
+public class Store {
+    private static ArrayList<ProductForSale> storeProducts = new ArrayList<>();
+    public static void main(String[] args) {
+
+        storeProducts.add(new ArtObject("Oil Painting", 1350,
+                "Impressionistic work by ABF painted in 2010"));
+
+        storeProducts.add(new ArtObject("Sculpture", 2000,
+                "Bronze work by JFK, produced in 1950"));
+
+        storeProducts.add(new Furniture("Desk", 2000,
+                "Marmelade Desk"));
+
+        storeProducts.add(new Furniture("Chair", 2000,
+                "Gaming chair"));
+
+        listProducts();
+
+        System.out.println("\nOrder 1");
+        var order1 = new ArrayList<OrderItem>();
+        addItemToOrder(order1,1,2);
+        addItemToOrder(order1,0,1);
+        printOrder(order1);
+
+        System.out.println("\nOrder 2");
+        var order2 = new ArrayList<OrderItem>();
+        addItemToOrder(order2,3,5);
+        addItemToOrder(order2,0,6);
+        addItemToOrder(order2,2,8);
+        printOrder(order2);
+
+    }
+
+    public static void  listProducts(){
+
+        for (var item : storeProducts)
+        {
+            System.out.println("-".repeat(30));
+            item.showDetails();
+        }
+    }
+
+    public static void addItemToOrder(ArrayList<OrderItem> order, int orderIndex, int qty){
+
+        order.add(new OrderItem(qty, storeProducts.get(orderIndex)));
+    }
+
+    public static void printOrder(ArrayList<OrderItem> order){
+
+        double salesTotal = 0;
+        for(var item : order){
+
+            item.product().printPricedItem(item.qty());
+            salesTotal += item.product().getSalesPrice(item.qty());
+        }
+        System.out.printf("Sales Total = $%6.2f %n", salesTotal);
+    }
+}
